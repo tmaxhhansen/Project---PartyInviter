4a25f088a376985ca0b137d113f1e0a2568371dd
Darren Seung Won <darren.hhansen@gmail.com>
2012-07-07 22:56:38 -0700

    working on InviterActivity

--- src/com/ACEApps/Inviter/InviterActivity.java
+++ src/com/ACEApps/Inviter/InviterActivity.java
@@ -30,6 +30,7 @@ import android.widget.ListView;
 import android.widget.TextView;
 import android.widget.Toast;
 
+@SuppressLint("ParserError")
 public class InviterActivity extends Activity implements OnClickListener {
 
 	private View view_background;
@@ -51,35 +52,36 @@ public class InviterActivity extends Activity implements OnClickListener {
 	private int mYear;
 	private int mMonth;
 	private int mDay;
-	
+
 	/*** People Picker ***/
 	private int peoplePicker_mselected = -1;
 	private Builder build_alertDialog;
-	private ArrayList<String> aList_name;
+	private String[] StringNames;
+	private ArrayList<String> aList_mobileNumber;
 
 	@Override
 	public void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.inviter_main);
-		
+
 		text_date = (TextView) findViewById(R.id.inviter_text_date);
 		text_description = (TextView) findViewById(R.id.inviter_text_description);
-		
+
 		view_background = (View) findViewById(R.id.inviter_background);
 		view_background.setOnClickListener(this);
-		
+
 		eText_date = (EditText) findViewById(R.id.inviter_edit_showdate);
 		eText_description = (EditText) findViewById(R.id.inviter_edit_description);
-		
+
 		button_send = (Button) findViewById(R.id.inviter_button_next);
 		button_send.setOnClickListener(this);
-		button_cancel= (Button) findViewById(R.id.inviter_button_cancel);
+		button_cancel = (Button) findViewById(R.id.inviter_button_cancel);
 		button_cancel.setOnClickListener(this);
 
 		button_peoplePicker = (Button) findViewById(R.id.inviter_button_peoplepicker);
 		button_peoplePicker.setOnClickListener(this);
-		
-		// button for datePicker connected to datepicker dialog 
+
+		// button for datePicker connected to datepicker dialog
 		button_datePicker = (Button) findViewById(R.id.inviter_button_datepicker);
 		button_datePicker.setOnClickListener(this);
 
@@ -92,27 +94,27 @@ public class InviterActivity extends Activity implements OnClickListener {
 	}
 
 	public void onClick(View _view) {
-		switch(_view.getId()){
+		switch (_view.getId()) {
 		case R.id.inviter_button_next:
-			
+
 			boolean stored = true;
-			try{
-			String date = eText_date.getText().toString();
-			String description = eText_description.getText().toString();
-			
-			SQLiteDAO entry = new SQLiteDAO(getApplicationContext());
-			
-			entry.open();
-			entry.create(date, description);
-			entry.close();
-			}catch (Exception e){
+			try {
+				String date = eText_date.getText().toString();
+				String description = eText_description.getText().toString();
+
+				SQLiteDAO entry = new SQLiteDAO(getApplicationContext());
+
+				entry.open();
+				entry.create(date, description);
+				entry.close();
+			} catch (Exception e) {
 				stored = false;
-				
-			}finally{
-				if(stored){
+
+			} finally {
+				if (stored) {
 					Intent s = new Intent(this, Inviter_SenderActivity.class);
 					startActivity(s);
-				}else{
+				} else {
 					Dialog popup = new Dialog(this);
 					popup.setTitle("Storing failed");
 					TextView tv = new TextView(this);
@@ -122,12 +124,13 @@ public class InviterActivity extends Activity implements OnClickListener {
 				}
 			}
 			break;
-			
+
 		case R.id.inviter_button_datepicker:
-			DatePickerDialog showDatePicker = new DatePickerDialog(this, mDateSetListener, mYear, mMonth, mDay);	
+			DatePickerDialog showDatePicker = new DatePickerDialog(this,
+					mDateSetListener, mYear, mMonth, mDay);
 			showDatePicker.show();
 			break;
-			
+
 		case R.id.inviter_button_peoplepicker:
 			peoplePickerHelper();
 			break;
@@ -136,7 +139,6 @@ public class InviterActivity extends Activity implements OnClickListener {
 
 	/*** protected methods ***/
 
-	
 	/**
 	 * DatePicker Dialog setup
 	 */
@@ -151,90 +153,124 @@ public class InviterActivity extends Activity implements OnClickListener {
 		}
 	};
 
-
 	/*** Private methods ***/
-	
+
 	private void hideKeyboard() {
 
 	}
-	
-	/**
-	 * Name: peoplePickerHelper()
-	 * Function: Helper for peoplePicker dialog
-	 */
-	private void peoplePickerHelper(){
-		build_alertDialog = new Builder(this);
-		build_alertDialog.setTitle("Select people to invite");
-		build_alertDialog.setCancelable(true);
-		
-	  ///TODO: Make a String arrayList to store names from conctacts
-		
-		final OnMultiChoiceClickListener onClick =
-	      new OnMultiChoiceClickListener() {
-	         public void onClick(final DialogInterface dialog,
-	                                      final int which, final boolean isChecked){
-	         Log.d("test", "which = " + which);
 
-	          if (isChecked) {
-	            if ((peoplePicker_mselected != -1) && (peoplePicker_mselected != which)) {
-	              final int oldVal = peoplePicker_mselected;
-	              final AlertDialog alert = (AlertDialog)dialog;
-	              final ListView list = alert.getListView();
-	              list.setItemChecked(oldVal, false);
-	            }
-	            Log.d("test", "which = " + which);
-	          } else
-	          	peoplePicker_mselected = -1;
-	        }
-	      };
-//	      build_alertDialog.setMultiChoiceItems(strings, null, onClick);
-	      build_alertDialog.setPositiveButton("Done", new DialogInterface.OnClickListener() {
-	       public void onClick(final DialogInterface dialog,
-	                                    final int which) {
-	        String message = null;
-	        if (peoplePicker_mselected == -1)
-	          message = "You didn't select anything.";
-	        else
-//	          message = "You selected '" + strings[mSelected] + "'";
-	        Toast.makeText(InviterActivity.this, message, Toast.LENGTH_LONG).show();
-	      }
-	    });
-	      build_alertDialog.show();
-	}
-	
-	
 	/**
-	 * Name: displayDate()
-	 * Function: display chosen date on the edit box 
-	 *           working with the datePicker button
+	 * Name: peoplePickerHelper() Function: Helper for peoplePicker dialog
+	 */
+	private void peoplePickerHelper() {
+		
+		String[] array_nameString = new String[getNameContact().size()];
+
+		if (array_nameString.length == 0) {
+			String message = null;
+			message = "You have no contact";
+			Toast.makeText(InviterActivity.this, message, Toast.LENGTH_LONG).show();
+		} else {
+
+			build_alertDialog = new Builder(this);
+			build_alertDialog.setTitle("Select people to invite");
+			build_alertDialog.setCancelable(true);
+
+			// /TODO: Make a String arrayList to store names from conctacts
+
+			final OnMultiChoiceClickListener onClick = new OnMultiChoiceClickListener() {
+				public void onClick(final DialogInterface dialog, final int which,
+						final boolean isChecked) {
+					Log.d("test", "which = " + which);
+
+					if (isChecked) {
+						if ((peoplePicker_mselected != -1)
+								&& (peoplePicker_mselected != which)) {
+							final int oldVal = peoplePicker_mselected;
+							final AlertDialog alert = (AlertDialog) dialog;
+							final ListView list = alert.getListView();
+							list.setItemChecked(oldVal, false);
+						}
+						Log.d("test", "which = " + which);
+					} else
+						peoplePicker_mselected = -1;
+				}
+			};
+			 build_alertDialog.setMultiChoiceItems(array_nameString, null, onClick);
+			build_alertDialog.setPositiveButton("Done",
+					new DialogInterface.OnClickListener() {
+						public void onClick(final DialogInterface dialog, final int which) {
+							String message = null;
+							if (peoplePicker_mselected == -1)
+								message = "You didn't select anything.";
+							else
+								// message = "You selected '" + strings[which] + "'";
+								Toast
+										.makeText(InviterActivity.this, message, Toast.LENGTH_LONG)
+										.show();
+						}
+					});
+			build_alertDialog.show();
+		}
+	}
+
+	/**
+	 * Name: displayDate() Function: display chosen date on the edit box working
+	 * with the datePicker button
 	 */
 	private void displayDate() {
 		eText_date.setText(new StringBuilder().append(mMonth + 1).append("/")
 				.append(mDay).append("/").append(mYear).append(" "));
 		// Month starts from 0. Need to add 1.
 	}
-	
-	
+
 	/**
-	 * Name: getNameContact
-	 * Function: retrieve names from the contact list 
+	 * Name: getNameContact Function: retrieve names from the contact list
 	 */
-	@SuppressLint("ParserError")
-	private String[] getNameContact(){
+	// @SuppressLint({ "ParserError", "ParserError" })
+	// @SuppressLint("ParserError")
+
+	private ArrayList<String> getNameContact() {
 		ContentResolver cr = getContentResolver();
-		Cursor cur = cr.query(ContactsContract.Contacts.CONTENT_URI, null, null, null, null);
-		
-		String tempName;
+		Cursor cur_findName = cr.query(ContactsContract.Contacts.CONTENT_URI, null,
+				null, null, null);
+
 		String tempId;
-		if(cur.getCount() > 0){
-			int cnt = 1; 
-			while(cur.moveToNext()){
-				
+		String mDecider = Integer
+				.toString(ContactsContract.CommonDataKinds.Phone.TYPE_MOBILE);
+		ArrayList<String> aList_name = new ArrayList<String>();
+
+		if (cur_findName.getCount() > 0) {
+			// int cnt = 1;
+			while (cur_findName.moveToNext()) {
+				tempId = cur_findName.getString(cur_findName
+						.getColumnIndex(ContactsContract.Contacts._ID));
+				if (Integer.parseInt(cur_findName.getString(cur_findName
+						.getColumnIndex(ContactsContract.Contacts.HAS_PHONE_NUMBER))) != 0) {
+					aList_name.add(cur_findName.getString(cur_findName
+							.getColumnIndex(ContactsContract.Contacts.DISPLAY_NAME)));
+
+					Cursor cur_findMobile = cr.query(
+							ContactsContract.CommonDataKinds.Phone.CONTENT_URI, null,
+							ContactsContract.CommonDataKinds.Phone.CONTACT_ID + " = ?",
+							new String[] { tempId }, null);
+
+					while (cur_findMobile.moveToNext()) {
+						String phonetype = cur_findMobile.getString(cur_findMobile
+								.getColumnIndex(ContactsContract.CommonDataKinds.Phone.TYPE));
+						if (phonetype.equals(mDecider)) {
+							aList_mobileNumber
+									.add(cur_findMobile.getString(cur_findMobile
+											.getColumnIndex(ContactsContract.CommonDataKinds.Phone.NUMBER)));
+							break;
+						}
+					}
+					cur_findMobile.close();
+				}
 			}
+
 		}
-		
-		return null;
+		return aList_name;
 	}
-          
 
 }
